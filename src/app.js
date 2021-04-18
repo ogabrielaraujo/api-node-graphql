9a386c533ef80349cccb1640a763d594f47e627d
Gabriel Araujo <gabriiel66@gmail.com>
2021-04-18 19:43:35 -0300

    logic

---
+++ src/app.js
@@ -0,0 +1,38 @@
+// # IMPORTS -/
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const { graphqlHTTP } = require('express-graphql')
+const graphqlSchema = require('./graphql/schema')
+const graphqlResolver = require('./graphql/resolvers')
+
+// # EXPRESS::INITIALIZE APP -/
+const app = express()
+
+// # ADD MIDDLEWARE -/
+app.use(bodyParser.json())
+app.use(cors())
+
+// # GRAPHQL::API SERVICE -/
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+)
+
+// # MONGOOSE::DATABASE SETUP -/
+mongoose
+  .connect(`mongodb://backend_admin:password@localhost:27017`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(3000, console.log('Connected to Port 3000.'))
+  })
+  .catch((err) => console.log(err))
